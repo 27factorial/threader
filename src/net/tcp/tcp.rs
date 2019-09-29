e119dd0a8322fd1f5f0c8125b4ac154f1d373352
Factorial <27factorial@gmail.com>
2019-09-29 13:17:32 -0700

    Add methods to TcpStream

--- src/net/tcp/tcp.rs
+++ src/net/tcp/tcp.rs
@@ -1,10 +1,11 @@
 use crate::reactor::{self, PollResource};
 use {
     futures::future,
-    mio::{net::TcpStream as MioTcpStream, Ready, PollOpt},
+    mio::{net::TcpStream as MioTcpStream, PollOpt, Ready},
     std::{
         io,
-        net::{SocketAddr, TcpStream as StdTcpStream, ToSocketAddrs},
+        net::{SocketAddr, Shutdown, TcpStream as StdTcpStream, ToSocketAddrs},
+        time::Duration,
     },
 };
 
@@ -46,5 +47,107 @@ impl TcpStream {
             Some(err) => Err(err),
             None => Ok(Self { io: poll_resource }),
         }
+    }
+
+    pub fn from_std(stream: StdTcpStream) -> io::Result<Self> {
+        let stream = MioTcpStream::from_stream(stream)?;
+        let io_waker = reactor::register(
+            &stream,
+            Ready::readable() | Ready::writable(),
+            PollOpt::edge(),
+        )?;
+        Ok(Self {
+            io: PollResource::new(stream, io_waker),
+        })
+    }
+
+    pub fn peer_addr(&self) -> io::Result<SocketAddr> {
+        self.io.get_ref().peer_addr()
+    }
+
+    pub fn local_addr(&self) -> io::Result<SocketAddr> {
+        self.io.get_ref().local_addr()
+    }
+
+    pub fn try_clone(&self) -> io::Result<TcpStream> {
+        let stream = self.io.get_ref().try_clone()?;
+        let io_waker = self.io.io_waker();
+        Ok(Self {
+            io: PollResource::new(stream, io_waker),
+        })
+    }
+
+    pub fn shutdown(&self, how: Shutdown) -> io::Result<()> {
+        self.io.get_ref().shutdown(how)
+    }
+
+    pub fn set_nodelay(&self, nodelay: bool) -> io::Result<()> {
+        self.io.get_ref().set_nodelay(nodelay)
+    }
+
+    pub fn nodelay(&self) -> io::Result<bool> {
+        self.io.get_ref().nodelay()
+    }
+
+    pub fn set_recv_buffer_size(&self, size: usize) -> io::Result<()> {
+        self.io.get_ref().set_recv_buffer_size(size)
+    }
+
+    pub fn recv_buffer_size(&self) -> io::Result<usize> {
+        self.io.get_ref().recv_buffer_size()
+    }
+
+    pub fn set_send_buffer_size(&self, size: usize) -> io::Result<()> {
+        self.io.get_ref().set_send_buffer_size(size)
+    }
+
+    pub fn send_buffer_size(&self) -> io::Result<usize> {
+        self.io.get_ref().send_buffer_size()
+    }
+
+    pub fn set_keepalive(&self, keepalive: Option<Duration>) -> io::Result<()> {
+        self.io.get_ref().set_keepalive(keepalive)
+    }
+
+    pub fn keepalive(&self) -> io::Result<Option<Duration>> {
+        self.io.get_ref().keepalive()
+    }
+
+    pub fn set_ttl(&self, ttl: u32) -> io::Result<()> {
+        self.io.get_ref().set_ttl(ttl)
+    }
+
+    pub fn ttl(&self) -> io::Result<u32> {
+        self.io.get_ref().ttl()
+    }
+
+    pub fn set_only_v6(&self, only_v6: bool) -> io::Result<()> {
+        self.io.get_ref().set_only_v6(only_v6)
+    }
+
+    pub fn only_v6(&self) -> io::Result<bool> {
+        self.io.get_ref().only_v6()
+    }
+
+    pub fn set_linger(&self, dur: Option<Duration>) -> io::Result<()> {
+        self.io.get_ref().set_linger(dur)
+    }
+
+    pub fn linger(&self) -> io::Result<Option<Duration>> {
+        self.io.get_ref().linger()
+    }
+
+    pub async fn peek(&self, buf: &mut [u8]) -> io::Result<usize> {
+        loop {
+            self.io.reregister(Ready::readable() | Ready::writable(), PollOpt::edge());
+            self.io.await_readable().await;
+
+            match self.io.get_ref().peek(buf) {
+                Ok(n) => return Ok(n),
+                Err(e) if e.kind() != io::ErrorKind::WouldBlock => return Err(e),
+                _ => (),
+            }
+        }
+
     }
 }
