12d499280b680334276717571dec3a2b171c7f7c
Factorial <27factorial@gmail.com>
2019-10-04 15:15:17 -0700

    Remove generic TcpStream::connect

--- src/net/tcp.rs
+++ src/net/tcp.rs
@@ -29,26 +29,7 @@ pub struct TcpStream {
 }
 
 impl TcpStream {
-    pub async fn connect<A: ToSocketAddrs>(addr: A) -> io::Result<Self> {
-        let mut last_err = None;
-        let addrs = addr.to_socket_addrs()?;
-
-        for addr in addrs {
-            match Self::connect_addr(&addr).await {
-                Ok(stream) => return Ok(stream),
-                Err(err) => last_err = Some(err),
-            }
-        }
-
-        Err(last_err.unwrap_or_else(|| {
-            io::Error::new(
-                io::ErrorKind::InvalidInput,
-                "could not resolve to any addresses",
-            )
-        }))
-    }
-
-    pub async fn connect_addr(addr: &SocketAddr) -> io::Result<Self> {
+    pub async fn connect(addr: &SocketAddr) -> io::Result<Self> {
         let stream = MioTcpStream::connect(&addr)?;
 
         // The stream will be writable when it's connected. We're assuming
