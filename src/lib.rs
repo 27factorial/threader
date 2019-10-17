296597c0f482866668c2cb733ea5b0e01ec7ba0b
Factorial <27factorial@gmail.com>
2019-10-17 13:44:09 -0700

    Add utils module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod executor;
 pub mod net;
 pub mod reactor;
+pub mod utils;
---
+++ src/utils/event_waiter.rs
@@ -0,0 +1,2 @@
+use parking_lot::{RwLock, RwLockWriteGuard as WriteGuard};
+use std::{cell::UnsafeCell, sync::Arc};
