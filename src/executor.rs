54dadb924a77c9547d3afdf381c4270c3f65d8a5
Factorial <27factorial@gmail.com>
2019-11-09 22:13:58 -0800

    Write documentation for `Executor` trait.

--- src/executor.rs
+++ src/executor.rs
@@ -1,5 +1,10 @@
 use futures::Future;
 
+/// A trait which includes a spawning method common to all
+/// future executors.
 pub trait Executor<F: Future> {
+    /// Spawns a future on this executor. In general, this
+    /// method should not fail or panic, but this is left up
+    /// to whatever is implementing the trait.
     fn spawn(&self, future: F);
 }
