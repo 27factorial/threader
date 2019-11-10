c92f66fa2eff7c544b86cb68c3694a43c2562d2c
Factorial <27factorial@gmail.com>
2019-11-09 22:13:42 -0800

    Write documentation for ThreadParker and ThreadUnparker

--- src/utils/thread_parker.rs
+++ src/utils/thread_parker.rs
@@ -1,4 +1,4 @@
-use crate::utils::debug_unreachable::debug_unreachable;
+use crate::utils::debug_utils::debug_unreachable;
 use crossbeam::utils::Backoff;
 use parking_lot::{Condvar, Mutex};
 use std::sync::{
@@ -6,39 +6,100 @@ use std::sync::{
     Arc,
 };
 
+/// Creates a new ThreadParker/ThreadUnparker pair. This is similar to creating
+/// a [`ThreadParker`] and calling [`unparker`](`ThreadParker::unparker`)
+/// on it.
 pub fn new2() -> (ThreadParker, ThreadUnparker) {
     let parker = ThreadParker::new();
     let unparker = parker.unparker();
     (parker, unparker)
 }
 
+/// A thread parking primitive. Based on [crossbeam]'s implementation, but
+/// uses the parking_lot [`Mutex`] and [`Condvar`] instead of the standard
+/// library's implementations.
+///
+/// # Example
+///
+/// ```
+/// use threader::utils::thread_parker;
+///
+/// fn main() {
+///     let (parker, unparker) = thead_parker::new2();
+/// }
+/// ```
 pub struct ThreadParker {
     inner: Arc<Inner>,
 }
 
 impl ThreadParker {
+    /// Creates a new `ThreadParker` instance.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use std::{
+    ///     thread,
+    ///     time::Duration,
+    /// };
+    /// use threader::utils::thread_parker::*;
+    ///
+    /// fn main() {
+    ///     let parker = ThreadParker::new();
+    ///     let unparker = parker.unparker();
+    ///
+    ///     let thread = thread::spawn(move || {
+    ///         thread::sleep(Duration::from_secs(5));
+    ///         unparker.unpark();
+    ///         println!("unparking thread");
+    ///     });
+    ///
+    ///     parker.park();
+    ///     println!("Unparked!");
+    /// }
+    /// ```
     pub fn new() -> ThreadParker {
         ThreadParker {
             inner: Arc::new(Inner::new()),
         }
     }
 
+    /// Creates an [`ThreadUnparker`] which is associated with
+    /// this `ThreadParker`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use threader::utils::thread_parker::*;
+    ///
+    /// fn main() {
+    ///     let parker = ThreadParker::new();
+    ///     let unparker = parker.unparker();
+    /// }
+    /// ```
     pub fn unparker(&self) -> ThreadUnparker {
         ThreadUnparker {
             inner: Arc::clone(&self.inner),
         }
     }
 
+    /// Parks the thread which calls this method. The thread can
+    /// then be woken up by calling [`unpark`] on the associated
+    /// [`ThreadUnparker`].
     pub fn park(&self) {
         self.inner.park();
     }
 }
 
+/// A thread unparking primitive. Used only to unpark threads which have
+/// previously been parked with [`ThreadParker::park`].
 pub struct ThreadUnparker {
     inner: Arc<Inner>,
 }
 
 impl ThreadUnparker {
+    /// Unparks the [`ThreadParker`] that this unparker
+    /// is associated with.
     pub fn unpark(&self) {
         self.inner.unpark();
     }
@@ -81,16 +142,16 @@ impl Inner {
                 }
             }
             NOTIFY => (),
-            _ => debug_unreachable(),
+            _ => unsafe { debug_unreachable() },
         }
 
-        // At this point, we know the state is NOTIFIED, and
+        // At this point, we know the state is NOTIFY, and
         // we're woken up, so we can set this to UNPARKED.
         self.notified.store(UNPARKED, Ordering::Release);
     }
 
     fn unpark(&self) {
-        let old = self.notified.swap(NOTIFY, Ordering::AcqRel);
+        self.notified.swap(NOTIFY, Ordering::AcqRel);
 
         // There's a short period when the parker is waiting
         // to lock the mutex and before it is unlocked when
